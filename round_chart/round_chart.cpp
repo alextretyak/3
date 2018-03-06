7fa381624fbfab61dd08fa42dcb20e210f3af850
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-03-06 23:54:44 +1000

    .

--- round_chart/round_chart.cpp
+++ round_chart/round_chart.cpp
@@ -1,16 +1,18 @@
-#include "CImg.h"
+п»ї#include "CImg.h"
 
 int main()
 {
 	/*
-	Выбрал 3 компоненты на пиксель, так как такая программа:
+	Р’С‹Р±СЂР°Р» 3 РєРѕРјРїРѕРЅРµРЅС‚С‹ РЅР° РїРёРєСЃРµР»СЊ, С‚Р°Рє РєР°Рє С‚Р°РєР°СЏ РїСЂРѕРіСЂР°РјРјР°:
 	```
-	cimg_library::CImg<unsigned char> img(1024, 1024); // A img = image:(1024, 1024) // А рис = изобр:(1024, 1024)
+	cimg_library::CImg<unsigned char> img(1024, 1024); // A img = image:(1024, 1024) // Рђ СЂРёСЃ = РёР·РѕР±СЂ:(1024, 1024)
 	img.save("round_chart.png");
 	```
-	вылетает с ошибкой [CImg] *** CImgIOException *** [instance(1024,1024,1,1,0274E040,non-shared)] CImg<unsigned char>::save_other(): Failed to save file 'round_chart.png'. Format is not natively supported, and no external commands succeeded.
+	РІС‹Р»РµС‚Р°РµС‚ СЃ РѕС€РёР±РєРѕР№ [CImg] *** CImgIOException *** [instance(1024,1024,1,1,0274E040,non-shared)] CImg<unsigned char>::save_other(): Failed to save file 'round_chart.png'. Format is not natively supported, and no external commands succeeded.
 	*/
-	cimg_library::CImg<unsigned char> img(1024, 1024, 1, 3); // A img = image:(1024, 1024, 3) // А рис = изобр:(1024, 1024, 3)
-	img.fill(255, 255, 255);                                 // img.fill((255, 255, 255))     // рис.заполнить((255, 255, 255))
-	img.save("round_chart.bmp");
+	cimg_library::CImg<unsigned char> img(1024, 1024, 1, 3); // A img = image:(1024, 1024, 3)               // Рђ СЂРёСЃ = РёР·РѕР±СЂ:(1024, 1024, 3)
+	img.fill(255, 255, 255);                                 // img.fill((255, 255, 255))                   // СЂРёСЃ.Р·Р°РїРѕР»РЅРёС‚СЊ((255, 255, 255))
+	unsigned char black_color[3] = {0, 0, 0};
+	img.draw_circle(512, 512, 256, black_color, 1, 0);       // img.draw_circle((512, 512), 256, (0, 0, 0)) // СЂРёСЃ.РЅР°СЂРёСЃРѕРІР°С‚СЊ_РєСЂСѓРі((512, 512), 256, (0, 0, 0))
+	img.save("round_chart.bmp");                             // в”Ђв”Ђ//в”Ђв”Ђ                                      // в”Ђв”Ђ//в”Ђв”Ђ
 }
