460bc6896fff8c98d38aeec0de73d15c9559bd46
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-03-07 12:57:46 +1000

    .

--- round_chart/round_chart.cpp
+++ round_chart/round_chart.cpp
@@ -20,18 +20,34 @@ int main()
 	unsigned char black_color[3] = {0, 0, 0};
 	img.draw_circle(512, 512, 256, black_color, 1, 0);        // img.draw_circle((512, 512), 256, (0, 0, 0))             // рис.круг((512, 512), 256, (0, 0, 0))
 	for (int i=0; i<24; i++) {                                // L(i) 24                                                 // Ц(и) 24
-		float angle = float((i+0.5)*2*M_PI/24.0);             //    A angle = (i+0.5)*2*PI/24                            //    А угол = (и+0.5)*2*ПИ/24
-		float dirx = sin(angle),                              //    A dir = (sin(angle),                                 //    А напр = (син(угол),
-		      diry =-cos(angle);                              //            -cos(angle))                                 //              кос(угол))
+		struct float2 {float x, y;};
+		auto f = [](float i) {                                //    F f(i)
+			float angle = float(i*2*M_PI/24.0);               //       A angle = i*2*PI/24                               //    А угол = (и+0.5)*2*ПИ/24
+			float2 r;                                         //       R (sin(angle), -cos(angle))
+			r.x = sin(angle);
+			r.y =-cos(angle);
+			return r;
+		};
+		float2 d = f(i+0.5f);                                 
 		char t[3];
-		img.draw_text(512 + int(dirx*200),                    //    img.draw_text(512 + dir*200, i)                      //    риc.текст(512 + напр*200, и)
-		              512 + int(diry*200),
+		img.draw_text(512 + int(d.x*200),                     //    img.draw_text(512 + f(i+0.5)*200, i)                 //    риc.текст(512 + напр*200, и)
+		              512 + int(d.y*200),
 		              (_itoa_s(i, t, 10), t), black_color);
-		float k = 1 + data[i]/max;
-		img.draw_line(512 + int(dirx*256),                    // img.draw_line(512 + dir*256, 512 + dir*256*data[i]/max) //    риc.линия(512 + напр*256, 512 + напр*256*данные[i]/макс)
-		              512 + int(diry*256),
-		   	          512 + int(dirx*256*k),
-		              512 + int(diry*256*k), black_color);
+		float2 d1 = f((float)i),
+		       d2 = f((float)i+1);
+		float k = 1 + data[i]/max;                            //    A k = 1 + data[i]/max
+		img.draw_line(512 + int(d1.x*256),                    //    img.draw_line(512 + d1*256  , 512 + d1*256*k)          //    риc.линия(512 + напр*256, 512 + напр*256*данные[i]/макс)
+		              512 + int(d1.y*256),
+		   	          512 + int(d1.x*256*k),
+		              512 + int(d1.y*256*k), black_color);
+		img.draw_line(512 + int(d1.x*256*k),                  //    img.draw_line(512 + d1*256*k, 512 + d2*256*k)
+		              512 + int(d1.y*256*k),
+		   	          512 + int(d2.x*256*k),
+		              512 + int(d2.y*256*k), black_color);
+		img.draw_line(512 + int(d2.x*256*k),                  //    img.draw_line(512 + d2*256*k, 512 + d2*256)
+		              512 + int(d2.y*256*k),
+		   	          512 + int(d2.x*256),
+		              512 + int(d2.y*256), black_color);
 	}
 
 	img.save("round_chart.bmp");                              // img.save(‘round_chart.bmp’)                             // рис.сохрани(‘round_chart.bmp’)
