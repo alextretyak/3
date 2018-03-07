e9d25582dacf728c088f19cca67e6ad7d0a60d7a
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-03-07 13:02:41 +1000

    .

--- round_chart/round_chart.cpp
+++ round_chart/round_chart.cpp
@@ -21,22 +21,22 @@ int main()
 	img.draw_circle(512, 512, 256, black_color, 1, 0);        // img.draw_circle((512, 512), 256, (0, 0, 0))             // рис.круг((512, 512), 256, (0, 0, 0))
 	for (int i=0; i<24; i++) {                                // L(i) 24                                                 // Ц(и) 24
 		struct float2 {float x, y;};
-		auto f = [](float i) {                                //    F f(i)
-			float angle = float(i*2*M_PI/24.0);               //       A angle = i*2*PI/24                               //    А угол = (и+0.5)*2*ПИ/24
-			float2 r;                                         //       R (sin(angle), -cos(angle))
+		auto f = [](float i) {                                //    F f(i)                                               //    Ф ф(и)
+			float angle = float(i*2*M_PI/24.0);               //       A angle = i*2*PI/24                               //       А угол = и*2*ПИ/24
+			float2 r;                                         //       R (sin(angle), -cos(angle))                       //       Р (син(угол), -кос(угол))
 			r.x = sin(angle);
 			r.y =-cos(angle);
 			return r;
 		};
 		float2 d = f(i+0.5f);                                 
 		char t[3];
-		img.draw_text(512 + int(d.x*200),                     //    img.draw_text(512 + f(i+0.5)*200, i)                 //    риc.текст(512 + напр*200, и)
+		img.draw_text(512 + int(d.x*200),                     //    img.draw_text(512 + f(i+0.5)*200, i)                 //    риc.текст(512 + ф(и+0.5)*200, и)
 		              512 + int(d.y*200),
 		              (_itoa_s(i, t, 10), t), black_color);
-		float2 d1 = f((float)i),
-		       d2 = f((float)i+1);
+		float2 d1 = f((float)i),                              //    A d1 = f(i),
+		       d2 = f((float)i+1);                                    d2 = f(i+1)
 		float k = 1 + data[i]/max;                            //    A k = 1 + data[i]/max
-		img.draw_line(512 + int(d1.x*256),                    //    img.draw_line(512 + d1*256  , 512 + d1*256*k)          //    риc.линия(512 + напр*256, 512 + напр*256*данные[i]/макс)
+		img.draw_line(512 + int(d1.x*256),                    //    img.draw_line(512 + d1*256  , 512 + d1*256*k)        //    риc.линия(...)
 		              512 + int(d1.y*256),
 		   	          512 + int(d1.x*256*k),
 		              512 + int(d1.y*256*k), black_color);
