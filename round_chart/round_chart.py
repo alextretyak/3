1da60eb39396f784392081970b25095928d746a2
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-03-07 17:42:25 +1000

    Не хочу выбирать день недели. Пусть будет сегодня! :)(:

--- round_chart/round_chart.py
+++ round_chart/round_chart.py
@@ -2,24 +2,24 @@
 
 times = [0]*24
 
-for i in range(1, 101):                                                                                             # L(i) 1..100
-    print('Processing page ' + str(i))
-    r = requests.get('http://habrahabr.ru/all/page'+str(i))                                                         #    A r = http:get(‘habrahabr.ru/all/page’i) E N   #or#   A r = http:get(‘habrahabr.ru/all/page’i) # А р = ппгт:(‘habrahabr.ru/all/page’н)
-    r.raise_for_status()                                                                                            #    r.throw_if_fail()                              #or#   [empty line]
-    html = r.text
-    i = 0
-    while True:
-        i = html.find('<span class="post__time">', i)
-        if i == -1:
-            break
+for i in range(1, 101):                                                    # L(i) 1..100
+    print('Processing page ' + str(i))                                     #    вывод(‘Processing page ’i)
+    r = requests.get('http://habrahabr.ru/all/page'+str(i))                #    A r = http:get(‘habrahabr.ru/all/page’i) E N   #or#   A r = http:get(‘habrahabr.ru/all/page’i) # А р = ппгт:(‘habrahabr.ru/all/page’н)
+    r.raise_for_status()                                                   #    r.throw_if_fail()                              #or#   [empty line]
+    html = r.text                                                          #
+    i = 0                                                                  #    A i = 0
+    while True:                                                            #    L
+        i = html.find('<span class="post__time">', i)                      #       i = html.find(‘<span class="post__time">’, i)
+        if i == -1:                                                        #       I i != N
+            break                                                          #          L.break
         i = html.find('</span>', i)
         time = html[:i][-5:]
 
         search_str = '<span class="post-stats__views-count">'
         start = html.find(search_str, i)
         i = html.find('</span>', start)
-        r = re.match('(\d+(?:,\d+])?)(k?)', html[start+len(search_str):i]) #    L(n, k) re:find_all[‘<span class="post-stats__views-count">(\d+,?\d+)(k?)</span>’](r.content)
-        n = float(r.group(1).replace(',','.'))                         #
+        r = re.match('(\d+(?:,\d+])?)(k?)', html[start+len(search_str):i]) # A r = re:match[‘(\d+(?:,\d+])?)(k?)’](html[start+search_str.len .< i])
+        n = float(r.group(1).replace(',','.'))
         if r.group(2) == 'k':
             n *= 1000
 
